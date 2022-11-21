5d985ef4cc7566bdaa89c0fe5c5339e8a20dc483
Thibaut <usualksm@gmail.com>
2022-11-21 10:38:19 +0100

    Test update 'Format file'

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,69 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
+import { bills } from "../fixtures/bills.js"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES_PATH } from "../constants/routes.js"
+import router from "../app/Router.js"
+import mockStore from "../__mocks__/store.js"
+import userEvent from "@testing-library/user-event";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    beforeEach(() => {
+      Object.defineProperty(window, "localStorage", {
+        value : localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+         type: "Employee",
+        })
+      );
+      
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+    });
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+      await waitFor(() => screen.getByTestId("icon-mail"))
+      const mailIcon = screen.getByTestId("icon-mail")
+      expect(mailIcon).toHaveClass("active-icon")
+    });
+
+    describe("When a file is selected through fil input", () => {
+      test("Then selecting image files (.jpg .jpeg .png) should work", () => {
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        };
+
+        const employeeNewBill = new NewBill({ 
+          document,
+          onNavigate,
+          store: mockStore,
+          localStorage: window.localStorage,
+        });
+
+        jest.spyOn(window, "alert").mockImplementation(() => {})
+        const fileInput = screen.getByTestId("file");
+
+        const handleChangeFile = jest.fn(employeeNewBill.handleChangeFile)
+        fileInput.addEventListener("change", (e) => handleChangeFile(e))
+
+        const file = new File(["test"], "test.png", { type: "image/png" })
+        userEvent.upload(fileInput, file);
+
+        expect(handleChangeFile).toHaveBeenCalled()
+        expect(window.alert).not.toHaveBeenCalled()
+        expect(fileInput.files[0]).toStrictEqual(file)
+      });
     })
   })
 })
